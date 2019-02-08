bc3fa432efc5bcc60456cbffb0525feb7631ea12
Christine Dodrill <me@christine.website>
2019-02-07 23:18:30 -0800

    create core

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(test), no_main)]
+#![cfg_attr(test, allow(unused_imports))]
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
